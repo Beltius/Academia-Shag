6f0422a8e015b0860a31dfbf8f6ad12b638a7600
Dima <Beltius@qip.ru>
2010-01-30 01:44:32 +0200

    доделал 2 урок и перевел файл на UTF8

--- Shag 2/Shag 2/Shag 2.cpp
+++ Shag 2/Shag 2/Shag 2.cpp
@@ -1,77 +1,124 @@
-// Shag 2.cpp : Defines the entry point for the console application.
-//
-#include "stdafx.h"
+﻿#include "stdafx.h"
 #include <iostream>
-#include <stdlib.h>
+#include <iomanip>
+#include <locale.h>
 using namespace std;
-
-int _tmain()
+int main()
 {
+Start:
+	setlocale(LC_ALL, "rus");
+	system ("color 0a");
 	int var;
-	int a;
+	int a, i=0, sum=0;
 	double b, c;
-	char s, d, z;
-	system("cls");
-	cout << "Vvedite chiot / nechiot 1" << endl;
-	cout << "Duimi / santimetri 2" << endl;
-	cout << "Perviy million 3" << endl;
-	cout << "Vihod 0" << endl;
+	char z;
+	//system("cls");
+	cout << "1. Напишите программу, проверяющую число, введенное с клавиатуры на четность." << endl;
+
+	cout << "2. Дано натуральное число а (a<100). Напишите программу, выводящую на экран";
+	cout << " количество цифр в этом числе и сумму этих цифр" << endl;
+	cout << "3. Известно, что 1 дюйм равен 2.54 см. Разработать приложение, переводящие дюймы";
+	cout << " в сантиметры и наоборот. Диалог с пользователем реализовать через систему меню." << endl;
+	cout << "4. Напишите программу, реализующую популярную телевизионную игру \"Кто хочет cтать миллионером\"." << endl; 
+	cout << "5. Выход" << endl;
 	cin >> var;
 	system("cls");
 	switch(var)
 	{
 		case 1: 
+			cout << "Ведите число : ";
 			cin>>a;
 			cout<<endl;
 				if (a%2==0)
 				{
-					cout << "chetnoe";
+					cout << "четное";
 					cout<<endl;
 				}
 				else
 				{
-					cout << "nechotnoe";
+					cout << "нечетное";
 					cout<<endl;
 				}
-			a=a/10+a%10;
-			cout << a << endl;
-			system("cls");
 		case 2:
-			cout << "santimetr v duim to vvedite s" << endl;
-			cout << "duim v santimetr to vvedite d" << endl;
+			cout << "Ведите число : ";
+			cin>>a;
+			cout<<endl;
+			if (a>9)
+				{
+					i = 2;
+				}
+  			else
+				{
+    				i = 1;
+				}
+			sum = a/10 + a%10;
+ 			cout <<"Сумма цифр :" << sum << endl;
+			cout <<"Количество цифр :" << i << endl;
+			goto Start;
+		case 3:
+			cout << "Чтобы перевести сантиметры в дюймы введите s" << endl;
+			cout << "Чтобы перевести дюймы в сантиметры введите  d" << endl;
 			cin>>z;
 			system("cls");
 			if ( z == 's')
 			{
-				cout << "Vvedite santimetry: ";
+				cout << "Введите сантиметры: ";
 				cin >> b;
 				cout << b*2.54 << endl;
 			}
 			if ( z == 'd')
 			{
-				cout << "Vvedite duimi: ";
+				cout << "Введите дюймы: ";
 				cin >> c;
 				cout << c/2.54 << endl;
 			}
-		case 3:
-			cout << "Skolko budet 2+2?"<<endl;
-			cout << "Varianti otvetov:"<< endl;
-			cout << "variant 1 :   10" << endl;
-			cout << "variant 2 :   6" << endl;
-			cout << "variant 3 :   4" << endl;
-			cout << "variant 4 :   8" << endl;
+			goto Start;
+		case 4:
+			cout << "Вопрос первый" << endl;
+			cout << "Сколько будет 2+2?"<<endl;
+			cout << "Варианты ответов:"<< endl;
+			cout << "Вариант 1 :   10" << endl;
+			cout << "Вариант 2 :   6" << endl;
+			cout << "Вариант 3 :   4" << endl;
+			cout << "Вариант 4 :   8" << endl;
 			cin >> a;
 			if (a==3)
 			{
-			cout << "Vi vigrali 1000000 $" << endl;
+			cout << "Вопрос второй" << endl;
+			cout << "Сколько будет 3+2?"<<endl;
+			cout << "Варианты ответов:"<< endl;
+			cout << "Вариант 1 :   10" << endl;
+			cout << "Вариант 2 :   5" << endl;
+			cout << "Вариант 3 :   4" << endl;
+			cout << "Вариант 4 :   8" << endl;
+			cin >> a;
+				if (a==2)
+				{
+					cout << "Сколько будет 3+3?"<<endl;
+					cout << "Варианты ответов:"<< endl;
+					cout << "Вариант 1 :   10" << endl;
+					cout << "Вариант 2 :   5" << endl;
+					cout << "Вариант 3 :   4" << endl;
+					cout << "Вариант 4 :   6" << endl;
+					cin >> a;
+					if (a==4)
+					{
+						cout << "Поздровляю!!!! Вы выйграли 1000000 $" << endl;
+					}
+				else
+				{
+				cout << "Вы проиграли." << endl;
+				break;
+				}
+				}
 			}
 			else
 			{
-			cout << "Vi ne vigrali 1000000 $" << endl;
+			cout << "Вы проиграли." << endl;
 			}
-		case 0:
+			goto Start;
+		case 5:
 			break;
 	}
 	return 0;
 }
-
