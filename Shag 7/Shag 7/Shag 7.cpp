688abf04d31c0e6b7a57bde3c7de08ab693aa1ad
Dima <Beltius@qip.ru>
2010-01-29 20:37:34 +0200

    добавил Д/з на 7 урок.
    под сомнением остается задача №2 т.к. все черные клетко генератор заполняет нулями.

--- Shag 7/Shag 7/Shag 7.cpp
+++ Shag 7/Shag 7/Shag 7.cpp
@@ -1,4 +1,4 @@
-#include "stdafx.h"
+﻿#include "stdafx.h"
 #include <iostream>
 #include <math.h>
 #include <iomanip>
@@ -7,24 +7,66 @@
 #include <time.h> 
 using namespace std;
 int main()
-{   
-    int sum=0;
+{   system ("color 0a");
+	setlocale (LC_ALL, "rus");
+	int sum=0, zadan, max=0;
+	const int	n=4;
    	int mr[3][4]; 
-	for(int i=0; i<3; i++)
+	Start:
+	cout << "1. Дан двухмерный массив размерностью 3X4. Необходимо найти колличество" <<endl;
+	cout << "элементов значение которых равно нулю." <<endl;
+	cout << "2. Дана квадратная матрица порядка n (n строк, n столбцов). Найти наибольшее" <<endl;
+	cout << "из значений элементов, расположенных в тёмно-синих частях матриц." <<endl;
+	cout << "Все массивы в данном домашнем задании заполняются случайным образом." <<endl;
+	cout << "3. Выход." <<endl;
+	cout << "Введите номер задания : "<<endl;
+	cin >> zadan;
+	switch(zadan)
 	{
-		for(int j=0; j<4;j++)
-		{
-			mr[i][j]=rand()%10;
-			
-        if (mr[i][j]==0)
-        {
-            sum+=1;
-        }
-        cout<<mr[i][j]<<" ";
-        }
+	case 1:
+		for(int i=0; i<3; i++)
+			{
+			for(int j=0; j<4;j++)
+				{
+				mr[i][j]=rand()%10;
+				if (mr[i][j]==0)
+					{
+         			   sum+=1;
+					}
+      			cout<< setw(5) << mr[i][j];
+       			}
 		cout<<"\n\n";
         }
-    cout<<sum<<" zeros in array\n\n";
-    system ("PAUSE");
-    return 0;
+   		cout << sum <<" - количество нулей в матрице" << endl;
+		goto Start;
+		break;
+	case 2:
+		/*cout << "Введите количество строк :";
+		cin >> n;*/
+		cout << endl;
+		int mb[n][n];
+		for(int i=0; i<n; i++)
+			{
+			for(int j=0; j<n;j++)
+				{
+				mb[i][j]=rand()%20;
+				if ((i+j)%2!=0)
+					{
+         			   if (mb[i][j]>max)
+					   {
+					  		 mb[i][j]=max;
+					   }
+					}
+				cout << setw(5) << mb[i][j];
+				}
+			cout << endl;
+        }
+		cout << max << " - наибольшее из значений элементов, расположенных в тёмно-синих частях матриц." << endl;
+   		goto Start;
+		break;
+	case 3:
+		break;
+	}
+	return 0;
 }
+
